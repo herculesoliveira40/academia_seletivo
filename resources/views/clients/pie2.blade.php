f2a7cffe8051c15141318b231a67a12a76271c05
herculesoliveira0 <herculesoliveira40@gmail.com>
2022-03-09 12:59:49 -0300

    Pie Chart edit

--- resources/views/clients/pie2.blade.php
+++ resources/views/clients/pie2.blade.php
@@ -7,23 +7,24 @@
   <head>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
-      google.charts.load("current", {packages:["corechart"]});
+      google.charts.load('current', {'packages':['corechart']});
       google.charts.setOnLoadCallback(drawChart);
+
       function drawChart() {
+
         var data = google.visualization.arrayToDataTable([
           ['Categoria Nome', 'Quantidade Clientes Categoria'],
           <?= $charData; ?>
+         
         ]);
 
-      var options = {
-        title: 'Clientes Categoria:',
-        legend: 'true',
-        pieSliceText: 'label',    
-        is3D: false,
-        pieStartAngle: 100,
-      };
+        var options = {
+          title: 'Clientes Categoria:',
+          is3D: false,
+        };
 
         var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
         chart.draw(data, options);
       }
     </script>
